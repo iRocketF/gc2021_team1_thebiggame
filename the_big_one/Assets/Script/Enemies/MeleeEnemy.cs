53da70878a99d26f3f472497eefd3fcdd1b85ed6
Laura Huovinen <laura.huovinen@outlook.com>
2021-07-07 16:21:07 +0300

    Melee enemy base
    - Always moves towards enemy
    - Hovers above ground

--- the_big_one/Assets/Script/Enemies/MeleeEnemy.cs
+++ the_big_one/Assets/Script/Enemies/MeleeEnemy.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MeleeEnemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private NavMeshAgent agent;
+
+    [SerializeField] private float pingPongLength = 0.5f;
+    [SerializeField] private float pingPongSpeed = 1f;
+    [SerializeField] private float minPingPongHeight = 1f;
+
+    [SerializeField] [Tooltip("Child game object of this parent")]
+    private GameObject enemyObject;
+    private GameObject player;
+
     void Start()
     {
-        
+        player = GameObject.Find("Player");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (player != null)
+        {
+            FollowPlayer();
+        }
+
+        PingPong();
+    }
+
+    void FollowPlayer()
+    {
+        Vector3 targetPos = player.transform.position;
+
+        transform.LookAt(targetPos);
+        agent.SetDestination(targetPos);
+    }
+
+    void PingPong()
+    {
+        float minimum = transform.position.y + minPingPongHeight;
+
+        float y = Mathf.PingPong(Time.time * pingPongSpeed, pingPongLength) + minimum;
+        enemyObject.transform.position = new Vector3(transform.position.x, y, transform.position.z);
     }
 }
