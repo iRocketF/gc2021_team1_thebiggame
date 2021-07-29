d98424b822bf304d2e6fcfc7518803056f172a60
Juho Mansikka <juho.mansikka@tuni.fi>
2021-07-29 15:40:38 +0300

    Started adding particles and emitters

--- the_big_one/Assets/HarpoonProjectile.cs
+++ the_big_one/Assets/HarpoonProjectile.cs
@@ -6,12 +6,18 @@ using UnityEngine;
 public class HarpoonProjectile : MonoBehaviour
 {
     public float thrust;
+    public GameObject particlePrefab;
 
     private Rigidbody rigidBody;
+    private ParticleSystem particles;
+    private GameObject pEmitter;
+    private bool particlesActive = true;
 
     private void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        pEmitter = Instantiate(particlePrefab, transform.position, transform.rotation);
+        particles = pEmitter.GetComponent<ParticleSystem>();
 
         AddForce();
     }
@@ -22,9 +28,25 @@ public class HarpoonProjectile : MonoBehaviour
     }
 
     public int damage;
-
+    
+    private void FixedUpdate()
+    {
+        if (particlesActive)
+        {
+            pEmitter.transform.position = transform.position;
+            pEmitter.transform.rotation = transform.rotation;
+        }
+    }
+    
     private void OnTriggerEnter(Collider other)
     {
+        if (particlesActive)
+        {
+            particlesActive = false;
+            particles.Stop();
+            Destroy(particles.transform.gameObject, 1f);
+        }
+
         if (other.gameObject.CompareTag("Enemy"))
         {
             // Debug.Log("Hit");
@@ -49,6 +71,13 @@ public class HarpoonProjectile : MonoBehaviour
 
     private void OnCollisionEnter(Collision other)
     {
+        if (particlesActive)
+        {
+            particlesActive = false;
+            particles.Stop();
+            Destroy(particles.transform.gameObject, 1f);
+        }
+
         if (!other.gameObject.CompareTag("Enemy"))
         {
             rigidBody.constraints = RigidbodyConstraints.FreezeAll;
--- the_big_one/Assets/Script/Enemies/RangedMissile.cs
+++ the_big_one/Assets/Script/Enemies/RangedMissile.cs
@@ -7,31 +7,38 @@ public class RangedMissile : MonoBehaviour
 {
     private float damage = 2f;
     public float thrust = 15f;
-
+    public GameObject particlePrefab;
     private Rigidbody rb;
+    private GameObject pEmitter;
+    private ParticleSystem particles;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        pEmitter = Instantiate(particlePrefab, transform.position, transform.rotation);
+        particles = pEmitter.GetComponent<ParticleSystem>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
         rb.AddForce(transform.forward * thrust);
+        pEmitter.transform.position = transform.position;
+        pEmitter.transform.rotation = transform.rotation;
     }
 
     private void OnCollisionEnter(Collision other)
     {
+        particles.Stop();
+        Destroy(particles.transform.gameObject, 1f);
+
         if (other.gameObject.CompareTag("Player"))
         {
             other.gameObject.GetComponent<PlayerHealth>().TakeDamage(damage);
 
             Debug.Log("Player took damage");
-            Destroy(gameObject);
         }
-        else
-        {
-            Destroy(gameObject);
-        }
+
+        Destroy(gameObject);
+
     }
 }
